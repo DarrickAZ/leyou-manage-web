66531ba1c3da2ef884c2cd32cae18df8162a1af3
DarrickAZ <zhwwhx@126.com>
2019-07-17 15:16:35 +0800

    ### update.

--- src/pages/item/MyBrand.vue
+++ src/pages/item/MyBrand.vue
@@ -1,44 +1,43 @@
 <template>
-  <div>
-    <v-card>
-      <v-card-title flat color="white">
-        <v-btn color="primary">新增</v-btn>
-        <!--空间隔离组件-->
-        <v-spacer />
-        <!--搜索框，与search属性关联-->
-        <v-text-field label="输入关键字搜索" v-model="search" append-icon="search" hide-details="true"/>
-      </v-card-title>
-      <v-data-table
-        :headers="headers"
-        :items="brands"
-        :pagination.sync="pagination"
-        :total-items="totalBrands"
-        :loading="loading"
-        class="elevation-1"
-      >
-        <template slot="items" slot-scope="props">
-          <td class="text-xs-center">{{ props.item.id }}</td>
-          <td class="text-xs-center">{{ props.item.name }}</td>
-          <td class="text-xs-center"><img v-if="props.item.image" :src="props.item.image" width="130" height="40"/></td>
-          <td class="text-xs-center">{{ props.item.letter }}</td>
-          <td class="text-xs-center">
-            <v-icon small class="mr-2" @click="editItem(props.item)">
-              edit
-            </v-icon>
-            <v-icon small @click="deleteItem(props.item)">
-              delete
-            </v-icon>
-          </td>
-        </template>
-      </v-data-table>
-    </v-card>
-  </div>
+  <v-card>
+    <v-card-title flat color="white">
+      <v-btn color="primary">新增</v-btn>
+      <!--空间隔离组件-->
+      <v-spacer />
+      <!--搜索框，与search属性关联-->
+      <v-text-field label="输入关键字搜索" append-icon="search" v-model="search" hide-details/>
+    </v-card-title>
+
+    <v-data-table
+      :headers="headers"
+      :items="brands"
+      :pagination.sync="pagination"
+      :total-items="totalBrands"
+      :loading="loading"
+      class="elevation-1"
+    >
+      <template slot="items" slot-scope="props">
+        <td class="text-xs-center">{{ props.item.id }}</td>
+        <td class="text-xs-center">{{ props.item.name }}</td>
+        <td class="text-xs-center"><img v-if="props.item.image" :src="props.item.image" width="130" height="40"/></td>
+        <td class="text-xs-center">{{ props.item.letter }}</td>
+        <td class="text-xs-center">
+          <v-icon small class="mr-2" @click="editItem(props.item)">
+            edit
+          </v-icon>
+          <v-icon small @click="deleteItem(props.item)">
+            delete
+          </v-icon>
+        </td>
+      </template>
+    </v-data-table>
+  </v-card>
 </template>
 
 <script>
   export default {
     name: "myBrand",
-    data () {
+    data() {
       return {
         totalBrands: 0, // 总条数
         brands: [], // 当前页品牌数据
@@ -50,60 +49,43 @@
           {text: '名称', align: 'center', value: 'name', sortable: false},
           {text: 'LOGO', align: 'center', value: 'image', sortable: false},
           {text: '首字母', align: 'center', value: 'letter'},
-          {text: '操作', align: 'center', value: 'id', sortable: false }
+          {text: '操作', align: 'center', value: 'id', sortable: false}
         ]
       }
     },
+    watch: {
+      pagination:{
+        deep: true, // 深度监视
+        handler(){
+          this.getDataFromServer();
+        }
+      },
+      search(){
+        this.pagination.page = 1;
+        this.getDataFromServer();
+      }
+    },
     methods: {
-      getDataFromServer(){ // 从服务端加载数据的函数
-        // 伪造演示数据
-        const brands = [
-          {
-            "id": 2032,
-            "name": "OPPO",
-            "image": "http://img10.360buyimg.com/popshop/jfs/t2119/133/2264148064/4303/b8ab3755/56b2f385N8e4eb051.jpg",
-            "letter": "O",
-            "categories": null
-          },
-          {
-            "id": 2033,
-            "name": "飞利浦（PHILIPS）",
-            "image": "http://img12.360buyimg.com/popshop/jfs/t18361/122/1318410299/1870/36fe70c9/5ac43a4dNa44a0ce0.jpg",
-            "letter": "F",
-            "categories": null
-          },
-          {
-            "id": 2034,
-            "name": "华为（HUAWEI）",
-            "image": "http://img10.360buyimg.com/popshop/jfs/t5662/36/8888655583/7806/1c629c01/598033b4Nd6055897.jpg",
-            "letter": "H",
-            "categories": null
-          },
-          {
-            "id": 2036,
-            "name": "酷派（Coolpad）",
-            "image": "http://img10.360buyimg.com/popshop/jfs/t2521/347/883897149/3732/91c917ec/5670cf96Ncffa2ae6.jpg",
-            "letter": "K",
-            "categories": null
-          },
-          {
-            "id": 2037,
-            "name": "魅族（MEIZU）",
-            "image": "http://img13.360buyimg.com/popshop/jfs/t3511/131/31887105/4943/48f83fa9/57fdf4b8N6e95624d.jpg",
-            "letter": "M",
-            "categories": null
+      getDataFromServer() { // 从服务端加载数据的函数
+        this.loading = true; // 加载数据
+        // 通过axios获取数据
+        this.$http.get("/item/brand/page", {
+          params: {
+            page: this.pagination.page, // 当前页
+            rows: this.pagination.rowsPerPage, // 每页条数
+            sortBy: this.pagination.sortBy, // 排序字段
+            desc: this.pagination.descending, // 是否降序
+            key: this.search // 查询字段
           }
-        ];
-        // 延迟一段时间，模拟数据请求时间
-        setTimeout(()=>{
-          this.brands = brands; // 赋值给品牌数组
-          this.totalBrands = brands.length; // 赋值数据总条数
-          this.loading = false; // 数据加载完成
-        }, 1000);
+        }).then(resp => { // 获取响应结果对象
+          this.totalBrands = resp.data.total; // 总条数
+          this.brands = resp.data.items; // 品牌数据
+          this.loading = false; // 加载完成
+        });
       }
     },
     // 渲染后执行
-    mounted(){
+    mounted() {
       this.getDataFromServer() // 调用数据初始化函数
     }
   }
