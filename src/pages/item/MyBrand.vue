fd1c3856081f704cccfd309ba83d2ccf83554a1d
DarrickAZ <zhwwhx@126.com>
2019-07-18 12:17:28 +0800

    ### modify for my brand.

--- src/pages/item/MyBrand.vue
+++ src/pages/item/MyBrand.vue
@@ -1,117 +1,167 @@
 <template>
   <v-card>
-    <v-card-title flat color="white">
-      <v-btn color="primary" @click="addBrand">新增品牌</v-btn>
-      <!--空间隔离组件-->
-      <v-spacer />
-      <!--搜索框，与search属性关联-->
-      <v-text-field label="输入关键字搜索" append-icon="search" v-model="search" hide-details/>
+    <v-card-title>
+      <v-btn @click="addBrand" color="primary">新增品牌</v-btn>
+      <v-spacer/>
+      <v-text-field
+        append-icon="search"
+        label="搜索"
+        single-line
+        hide-details
+        v-model="search"
+      />
     </v-card-title>
-
+    <v-divider/>
     <v-data-table
       :headers="headers"
-      :items="brands"
+      :items="items"
       :pagination.sync="pagination"
-      :total-items="totalBrands"
+      :total-items="totalItems"
       :loading="loading"
       class="elevation-1"
     >
       <template slot="items" slot-scope="props">
         <td class="text-xs-center">{{ props.item.id }}</td>
         <td class="text-xs-center">{{ props.item.name }}</td>
-        <td class="text-xs-center"><img v-if="props.item.image" :src="props.item.image" width="130" height="40"/></td>
+        <td class="text-xs-center"><img v-if="!!props.item.image" width="102" height="36" :src="props.item.image"/></td>
         <td class="text-xs-center">{{ props.item.letter }}</td>
-        <td class="text-xs-center">
-          <v-icon small class="mr-2" @click="editItem(props.item)">
-            edit
-          </v-icon>
-          <v-icon small @click="deleteItem(props.item)">
-            delete
-          </v-icon>
+        <td class="justify-center layout px-0">
+          <v-btn icon @click="editBrand(props.item)">
+            <i class="el-icon-edit"/>
+          </v-btn>
+          <v-btn icon @click="deleteBrand(props.item)">
+            <i class="el-icon-delete"/>
+          </v-btn>
         </td>
+      </template>
+      <template slot="expand" slot-scope="props">
+        <v-card flat>
+          <v-card-text>Peek-a-boo!</v-card-text>
+        </v-card>
+      </template>
+      <template slot="no-data">
+        <v-alert :value="true" color="error" icon="warning">
+          对不起，没有查询到任何数据 :(
+        </v-alert>
+      </template>
+      <template slot="pageText" slot-scope="props">
+        共{{props.itemsLength}}条,当前:{{ props.pageStart }} - {{ props.pageStop }}
       </template>
     </v-data-table>
 
-    <!--弹出的对话框-->
-    <v-dialog max-width="500" v-model="show" persistent>
+    <v-dialog v-model="show" max-width="600" scrollable v-if="show">
       <v-card>
-        <!--对话框的标题-->
-        <v-toolbar dense dark color="primary">
-          <v-toolbar-title>新增品牌</v-toolbar-title>
+        <v-toolbar dark dense color="primary">
+          <v-toolbar-title>{{isEdit ? '修改品牌' : '新增品牌'}}</v-toolbar-title>
+          <v-spacer/>
+          <v-btn icon @click="show = false">
+            <v-icon>close</v-icon>
+          </v-btn>
         </v-toolbar>
-        <!--对话框的内容，表单-->
-        <v-card-text class="px-5">
-          我是表单
+        <v-card-text class="px-5 py-2">
+          <!-- 表单 -->
+          <my-brand-form :oldBrand="brand" :isEdit="isEdit" @close="show = false" :reload="getDataFromApi"/>
         </v-card-text>
       </v-card>
     </v-dialog>
-
   </v-card>
+
 </template>
 
 <script>
+  import MyBrandForm from './MyBrandForm'
+  import {brandData} from '../../mockDB'
+
   export default {
     name: "myBrand",
+    components: {
+      MyBrandForm
+    },
     data() {
       return {
-        totalBrands: 0, // 总条数
-        brands: [], // 当前页品牌数据
-        search: "", // 查询关键字
-        loading: true, // 是否在加载中
-        pagination: {}, // 分页信息
-        headers: [ // 头信息
+        search: '',// 过滤字段
+        totalItems: 0,// 总条数
+        items: [],// 表格数据
+        loading: true,
+        pagination: {},// 分页信息
+        headers: [// 表头
           {text: 'id', align: 'center', value: 'id'},
-          {text: '名称', align: 'center', value: 'name', sortable: false},
-          {text: 'LOGO', align: 'center', value: 'image', sortable: false},
-          {text: '首字母', align: 'center', value: 'letter'},
+          {text: '名称', align: 'center', sortable: false, value: 'name'},
+          {text: 'LOGO', align: 'center', sortable: false, value: 'image'},
+          {text: '首字母', align: 'center', value: 'letter', sortable: true,},
           {text: '操作', align: 'center', value: 'id', sortable: false}
         ],
-        show:false,//用于控制dialog的显示
+        show: false,// 是否弹出窗口
+        brand: {}, // 品牌信息
+        isEdit: false // 判断是编辑还是新增
       }
     },
     watch: {
-      pagination:{
-        deep: true, // 深度监视
-        handler(){
-          this.getDataFromServer();
+      pagination: {
+        handler() {
+          this.getDataFromApi();
+        },
+        deep: true
+      },
+      search: {
+        handler() {
+          this.getDataFromApi();
         }
       },
-      search(){
-        this.pagination.page = 1;
-        this.getDataFromServer();
+      show(val, oldVal) {
+        // 表单关闭后重新加载数据
+        if (oldVal) {
+          this.getDataFromApi();
+        }
       }
+    },
+    mounted() {
+      this.getDataFromApi();
     },
     methods: {
-      getDataFromServer() { // 从服务端加载数据的函数
-        this.loading = true; // 加载数据
-        // 通过axios获取数据
-        this.$http.get("/item/brand/page", {
-          params: {
-            page: this.pagination.page, // 当前页
-            rows: this.pagination.rowsPerPage, // 每页条数
-            sortBy: this.pagination.sortBy, // 排序字段
-            desc: this.pagination.descending, // 是否降序
-            key: this.search // 查询字段
-          }
-        }).then(resp => { // 获取响应结果对象
-          this.totalBrands = resp.data.total; // 总条数
-          this.brands = resp.data.items; // 品牌数据
-          this.loading = false; // 加载完成
-        });
-      },
-      addBrand(){
-        // 控制弹窗可见：
+      addBrand() {
+        this.brand = {};
+        this.isEdit = false;
         this.show = true;
+      },
+      editBrand(item) {
+        this.brand = item;
+        this.isEdit = true;
+        this.show = true;
+        // 查询商品分类信息，进行回显
+        this.$http.get("/item/category/bid/" + item.id)
+          .then(resp => {
+            this.brand.categories = resp.data;
+          })
+
+      },
+      deleteBrand(item) {
+        this.$message.confirm('此操作将永久删除该品牌, 是否继续?').then(() => {
+          // 发起删除请求
+          this.$http.delete("/item/brand?id=" + item.id,)
+            .then(() => {
+              // 删除成功，重新加载数据
+              this.$message.success("删除成功！");
+              this.getDataFromApi();
+            })
+        }).catch(() => {
+          this.$message.info("删除已取消！");
+        });
+
+      },
+      getDataFromApi() {
+        this.loading = true;
+        // 200ms后返回假数据
+        window.setTimeout(() => {
+          this.items = brandData.slice(0,4);
+          this.totalItems = 100
+          this.loading = false;
+        }, 200)
       }
-    },
-    // 渲染后执行
-    mounted() {
-      this.getDataFromServer() // 调用数据初始化函数
     }
   }
 </script>
 
-<!-- scoped:当前样式只作用于当前组件的节点 -->
 <style scoped>
 
 </style>
