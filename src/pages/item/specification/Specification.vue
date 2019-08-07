452e4ac89d16eba4a3bfcd3354596c0e08f85315
DarrickAZ <zhwwhx@126.com>
2019-08-07 14:38:32 +0800

    ### push project.

--- src/pages/item/specification/Specification.vue
+++ src/pages/item/specification/Specification.vue
@@ -17,7 +17,7 @@
           <v-card class="elevation-0" v-show="currentNode.id">
               <v-card-title v-if="!currentNode.path">
                 请先选择一个分类
-              </v-card-title>         
+              </v-card-title>
             <v-breadcrumbs dense v-else>
                 <v-icon slot="divider">chevron_right</v-icon>
                 <v-breadcrumbs-item
@@ -53,15 +53,36 @@ export default {
   methods: {
     // 分类点击后的弹窗
     handleClick(node) {
-      // 判断点击的节点是否是父节点（只有点击到叶子节点才会展示规格）
-      if (!node.isParent) {
-        // 把当前点击的节点记录下来
-        this.currentNode = node;
-        // 显示规格组
-        this.showGroup = true;
-        // 记录选中的规格组，默认为空
-        this.group = {};
-      }
+        // 判断点击的节点是否是父节点（只有点击到叶子节点才会弹窗）
+        if (!node.isParent) {
+            // 如果是叶子节点，那么就发起ajax请求，去后台查询商品规格数据。
+            this.$http.get("/item/spec/" + node.id)
+                .then(resp => {
+                    // 查询成功后，把响应结果赋值给specifications属性，Vue会进行自动渲染。
+                    this.specifications = resp.data;
+                    // 记录下此时的规格数据，当页面撤销修改时，用来恢复原始数据
+                    this.oldSpec = resp.data;
+                    // 打开弹窗
+                    this.dialog = true;
+                    // 标记此时要进行修改操作
+                    this.isInsert = false;
+                })
+                .catch(() => {
+                    // 如果没有查询成功，那么询问是否添加规格
+                    this.$message.confirm('该分类还没有规格参数，是否添加?')
+                        .then(() => {
+                            // 如果要添加，则将specifications初始化为空
+                            this.specifications = [{
+                                group: '',
+                                params: []
+                            }];
+                            // 打开弹窗
+                            this.dialog = true;
+                            // 标记为新增
+                            this.isInsert = true;
+                        })
+                })
+        }
     },
     selectGroup(group){
       // 记录选中的分组
